feee09efdc632576aa73fd32b9a525b4f15a80ce
bailey watkins <bailey.watkins@rocketmail.com>
2021-04-14 16:34:46 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog");
+
+
+
+function mondayWork(activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}.`)
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+
+function wrapAdjective(decoration="*") {
+    return function(adjective="special"){
+       return `You are ${decoration}${adjective}${decoration}!`
+    }
+}
